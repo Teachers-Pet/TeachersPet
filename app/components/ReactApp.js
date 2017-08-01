b081b669015b106a2fe6487bec4ff722ec681b80
Henrietta Sharpe <henrietta_sharpe@yahoo.com>
2017-08-01 19:57:52 -0400

    fixed react component

--- app/components/ReactApp.js
+++ app/components/ReactApp.js
@@ -1,5 +1,5 @@
-var React = require("react");
-var helpers = require("./utils/helpers");
+var React = require('react');
+var helpers = require('./utils/helpers');
 
 var ReactApp = React.createFactory(
   React.createClass({
@@ -18,25 +18,26 @@ var ReactApp = React.createFactory(
         }.bind(this)
       );
     },
+
     render: function() {
       return (
-        <div className="row">
-          <div className="col s12 student-list-pnl">
-            <div className="card-panel white">
-              <ul className="collection">
-                {this.state.students.map((students, i) => {
-                  return (
-                    <a href="#student-summary-modal" id={students.id} key={i} className="collection-item name listed-student">
+        <div className="collection with-header center">
+          <div className="collection-header">
+            <h5>
+              Students
+              <a href="#add-student-modal" className="">
+                <i className="material-icons orange-text">add_circle</i>
+              </a>
+            </h5>
+          </div>
+          {this.state.students.map((students, i) => {
+            return (
+              <a href="#student-summary-modal" id={students.id} key={i} className="collection-item name listed-student">
                 <span className="showId">{students.id}</span> | <span className="showStudentName">{students.name}</span>
               </a>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
+            );
+          })}
         </div>
-
-
       );
     }
   })
