627a8b3b6660a4fb1d9972276c359df3c1311c77
indiaaeevans <indiaaeevans@gmail.com>
2017-08-01 20:04:56 -0400

    Update classSummary.js

--- public/assets/js/classSummary.js
+++ public/assets/js/classSummary.js
@@ -1,59 +1,8 @@
 $(document).ready(function() {
-  // store logged in teacher's id
-  var id;
-  // initialize materialize
-  $(".modal").modal();
-  $(".button-collapse ").sideNav();
-  $(".button-collapse").sideNav("hide");
-  $(".dropdown-button").dropdown();
-
-  $(".datepicker").pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: "Today",
-    clear: "Clear",
-    close: "Ok",
-    closeOnSelect: false // Close upon selecting a date,
-  });
-
+  // store logged in teacher's id and name
+  var id, teacherName;  
   $.get("/api/teacher_data", function(data) {
     var teacherName = data.name;
-    id = data.id;
-    $("#teacher-name").text(teacherName + "'s");
-  });
-
-  //add event button
-  $("#submit-event-btn").on("click", function(event) {
-    var eventName = $("#event-name").val().trim();
-    var eventDate = $("#event-date").val().trim();
-  });
-
-  //-------add student button-----------
-  $("#add-student-btn").on("click", function(event) {
-    var newStudent = {
-      name: $("#first-name").val().trim() + " " + $("#last-name").val().trim(),
-      imgUrl: $("#img-url").val().trim(),
-      email: $("#email").val().trim(),
-      TeacherId: id
-    };
-    // send an AJAX POST-request with jQuery
-    $.post("/api/students", newStudent)
-      // on success, run this callback
-      .done(function(data) {
-        alert("Adding student...");
-      });
-
-    // empty each input box by replacing the value with an empty string
-    $("#first-name").val("");
-    $("#last-name").val("");
-    $("#email").val("");
-    location.href = "/class";
-  });
-  // store logged in teacher's id
-  var id, teacherName;
-
-  $.get("/api/teacher_data", function(data) {
-    teacherName = data.name;
     id = data.id;
     $("#teacher-name").text(teacherName + "'s");
   });
@@ -64,7 +13,7 @@ $(document).ready(function() {
   $(".button-collapse").sideNav("hide");
   $(".dropdown-button").dropdown();
 
-  // for adding events modal (models not complete for this feature)
+  // date picker for events modal
   $(".datepicker").pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
